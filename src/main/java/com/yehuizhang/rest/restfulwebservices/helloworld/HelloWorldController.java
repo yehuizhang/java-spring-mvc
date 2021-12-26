bf2f17568d19b0fcfbd5ee709fda66b07814901a
Yehui Zhang <yehuizhang@outlook.com>
2021-12-25 21:26:02 -0800

    i18n

--- src/main/java/com/yehuizhang/rest/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/yehuizhang/rest/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,24 @@
 package com.yehuizhang.rest.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-i18n")
+    public String helloWorldI18n() {
+        return messageSource.getMessage("hello.world", null, LocaleContextHolder.getLocale());
     }
 
     @GetMapping(path = "/hello-world-bean")
